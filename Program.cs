72bb49c765ac46ab47bb73e49af1a4e16709b171
Jamie Hankins <jamiehankin@gmail.com>
2014-04-27 14:50:57 +0100

    Generate 1000 WebClients

--- Program.cs
+++ Program.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Net;
 using System.Text;
 using System.Threading.Tasks;
 
@@ -10,6 +11,25 @@ namespace Lexa
     {
         static void Main(string[] args)
         {
+            var list = generateWebClients(1000);
+            
+            Console.WriteLine("Done");
+            Console.ReadLine();
+        }
+
+        static List<WebClient> generateWebClients(int amount)
+        {
+            ServicePointManager.DefaultConnectionLimit = int.MaxValue;
+
+            var webClientList = new List<WebClient>();
+
+            for (int i = 0; i < amount; i++)
+            {
+                var webclient = new WebClient();
+                webclient.Proxy = null;
+                webClientList.Add(webcli);
+            }
+            return webClientList;
         }
     }
 }
