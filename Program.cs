91d406a006af73c2cddd626c82f4fc8725d132bc
Jamie Hankins <jamiehankin@gmail.com>
2014-04-28 14:46:40 +0100

    Clean up our Async thanks to @Rohansi

--- Program.cs
+++ Program.cs
@@ -11,17 +11,6 @@ namespace Lexa
 {
     class Program
     {
-        //Thanks to -- http://stackoverflow.com/a/22425211/1365365
-        public static void WriteToBinaryFile<T>(string filePath, T objectToWrite, bool append = false)
-        {
-            using (Stream stream = File.Open(filePath, append ? FileMode.Append : FileMode.Create))
-            {
-                var binaryFormatter = new System.Runtime.Serialization.Formatters.Binary.BinaryFormatter();
-                binaryFormatter.Serialize(stream, objectToWrite);
-            }
-        }
-
-        [Serializable]
         public class Site
         {
             public int Index;
@@ -36,86 +25,100 @@ namespace Lexa
             }
         }
 
-        public static List<TimeSpan> TimeSpans = new List<TimeSpan>(); 
-        public static List<Site> ProcessedSites = new List<Site>();
+        public static int poo = 0;
+
         static void Main(string[] args)
         {
             var sites = new List<Site>();
             
             ServicePointManager.DefaultConnectionLimit = int.MaxValue;
 
-            using (var sw = new StreamReader(@"top-1m.csv"))
-            {
-                sites.AddRange(from ln in sw.ReadToEnd().Split('\n') where ln != "" let lnno = Convert.ToInt32(ln.Split(',')[0]) let site = ln.Split(',')[1] select new Site(lnno, site));
-            }
+            //using (var sw = new StreamReader(@"top-1m.csv"))
+            //{
+            //    sites.AddRange(from ln in sw.ReadToEnd().Split('\n') where ln != "" let lnno = Convert.ToInt32(ln.Split(',')[0]) let site = ln.Split(',')[1] select new Site(lnno, site));
+            //}
 
+            sites.Add(new Site(1, "google.com"));
+            sites.Add(new Site(2, "google.co.uk"));
+            sites.Add(new Site(3, "jamiehankins.co.uk"));
+            sites.Add(new Site(4, "bing.com"));
+            sites.Add(new Site(5, "reddit.com"));
 
-            const int sitesPerATask = 100;
+            var done = scrapeTask(sites);
+            Console.WriteLine("Done!!!!!");
+            Console.ReadLine();
+
+            //TimeSpan avg = new TimeSpan(0);
+            //foreach (var timespan in TimeSpans)
+            //{
+            //    avg = avg + timespan;
+            //}
+            //var start = DateTime.Now;
+            //var eta = (avg.Milliseconds / TimeSpans.Count) * (totalTasks - TimeSpans.Count);
+
+            //var end = DateTime.Now;
+            //var ts = end - start;
+            //TimeSpans.Add(ts);
+
+            //Console.WriteLine("ETA: {0}", eta * 1000);
+            //Console.WriteLine("Tasks complete");
+        }
+
+        private static List<Site> scrapeTask(List<Site> sites)
+        {
+            const int sitesPerATask = 1;
             const int sleepTime = 200;
             int taskCount = sites.Count / sitesPerATask;
             Console.WriteLine("Using {0} tasks each crawling a total of {1} sites and sleeping inbetween for {2}MS", taskCount, sitesPerATask, sleepTime);
 
+            var taskResults = new List<Site>();
+
             for (var i = 0; i < taskCount; i++)
             {
                 var taskList = sites.Skip(i * sitesPerATask).Take(sitesPerATask);
-                Console.WriteLine(taskList.Count());
-                runLoop(taskList.ToList(), i, taskCount);
+                taskResults = ListProcessor(taskList.ToList(), i + 1, taskCount).Result;
                 Thread.Sleep(sleepTime);
             }
-            while (ProcessedSites.Count != sites.Count)
-            {
-                Thread.Sleep(1000);
-            }
 
-            Console.WriteLine("Done!!!!!");
-            WriteToBinaryFile("output.bin", ProcessedSites);
-            Console.ReadLine();
+            return taskResults;
         }
 
-        private static async void runLoop(List<Site> sites, int taskID, int totalTasks)
+        private static async Task<List<Site>>  ListProcessor(List<Site> sites, int taskID, int totalTasks)
         {
-            var start = DateTime.Now;
             Console.WriteLine("Running Task {0} of {1}", taskID, totalTasks);
             var tasks = sites.Select(site => ProcessSite(site, taskID)).ToList();
-            
             await Task.WhenAll(tasks);
-            
-            var end = DateTime.Now;
-            var ts = end - start;
-            TimeSpans.Add(ts);
-
-            TimeSpan avg = new TimeSpan(0);
-            foreach (var timespan in TimeSpans)
-            {
-                avg = avg + timespan;
-            }
-            var eta = (avg.Milliseconds/TimeSpans.Count)*(totalTasks - TimeSpans.Count);
-            Console.WriteLine("ETA: {0}", eta * 1000);
-
-            foreach (var task in tasks)
-            {
-                ProcessedSites.Add(task.Result);
-            }
-
-            Console.WriteLine("Tasks complete");
+            return tasks.Select(t => t.Result).ToList();
         }
 
         private static async Task<Site> ProcessSite(Site site, int taskID)
         {
             var wc = new WebClient();
             wc.Proxy = null;
+            
+
+                int timeout = 5000;
             try
             {
-                site.Data = await wc.DownloadStringTaskAsync(new Uri("http://" + site.SiteName));
+                Task<string> ts = wc.DownloadStringTaskAsync(new Uri("http://" + site.SiteName));
+                if (await Task.WhenAny(ts, Task.Delay(timeout)) == ts)
+                {
+                    site.Data = ts.Result;
+                }
+                else
+                {
+                    site.Error = "Timed out";
+                    return site;
+                }
             }
-            catch(Exception ex)
+            catch (Exception ex)
             {
                 Console.WriteLine("Site: {0} has errored!", site.SiteName);
                 site.Error = ex.ToString();
             }
-            site.Headers = wc.ResponseHeaders;
 
-            Console.WriteLine("(T:{0}) Completed {1}", taskID, site.SiteName);
+            site.Headers = wc.ResponseHeaders;
+            Console.WriteLine("(T:{0} - Site: {1}) Completed {2}", taskID,site.Index, site.SiteName);
             return site;
         }
     }
