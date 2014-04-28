4010aed7beb825f8454af6d125cf8357e96973c8
Jamie Hankins <jamiehankin@gmail.com>
2014-04-28 12:07:27 +0100

    Implement storing the returned data in the class. Use LINQ for the CSV and add some values for task size

--- Program.cs
+++ Program.cs
@@ -14,7 +14,9 @@ namespace Lexa
         public class Site
         {
             public int Index;
+            public string Data;
             public string SiteName;
+            public string Error;
             public WebHeaderCollection Headers{ get; set; }
             public Site(int index, string site)
             {
@@ -23,51 +25,69 @@ namespace Lexa
             }
         }
 
-
+        public static int Complete = 0;
+        public static List<TimeSpan> TimeSpans = new List<TimeSpan>(); 
         static void Main(string[] args)
         {
             var sites = new List<Site>();
+            
+            ServicePointManager.DefaultConnectionLimit = int.MaxValue;
 
             using (var sw = new StreamReader(@"top-1m.csv"))
             {
-                foreach (string ln in sw.ReadToEnd().Split('\n'))
-                {
-                    if (ln != "")
-                    {
-                        int lnno = Convert.ToInt32(ln.Split(',')[0]);
-                        string site = ln.Split(',')[1];
-                        Site s = new Site(lnno, site);
-                        sites.Add(s);
-                    }
-                }
+                sites.AddRange(from ln in sw.ReadToEnd().Split('\n') where ln != "" let lnno = Convert.ToInt32(ln.Split(',')[0]) let site = ln.Split(',')[1] select new Site(lnno, site));
             }
 
-            ServicePointManager.DefaultConnectionLimit = int.MaxValue;
-            
-            runLoop(sites);
+
+            const int sitesPerATask = 100;
+            const int sleepTime = 200;
+            int taskCount = sites.Count / sitesPerATask;
+            Console.WriteLine("Using {0} tasks each crawling a total of {1} sites and sleeping inbetween for {2}MS", taskCount, sitesPerATask, sleepTime);
+
+            for (var i = 0; i <= taskCount; i++)
+            {
+                var taskList = sites.Skip(i * sitesPerATask).Take(sitesPerATask);
+                Console.WriteLine(taskList.Count());
+                runLoop(taskList.ToList(), i, taskCount);
+                Thread.Sleep(sleepTime);
+            }            
             
             Console.WriteLine("Done");
             Console.ReadLine();
         }
 
-        private static async void runLoop(List<Site> sites)
+        private static async void runLoop(List<Site> sites, int taskID, int totalTasks)
         {
-            var tasks = sites.Select(site => ProcessSite(site)).ToList();
+            var start = DateTime.Now;
+
+            Console.WriteLine(start.ToString());
+            Console.WriteLine("Running Task {0} of {1}", taskID, totalTasks);
+            var tasks = sites.Select(site => ProcessSite(site, taskID)).ToList();
             await Task.WhenAll(tasks);
+            
+            var end = DateTime.Now;
+            var ts = end - start;
+            TimeSpans.Add(ts);
+
             Console.WriteLine("Tasks complete");
         }
 
-        private static async Task<Site> ProcessSite(Site site)
+        private static async Task<Site> ProcessSite(Site site, int taskID)
         {
-            Console.WriteLine("Starting: {0}", site.SiteName);
-            WebClient wc = new WebClient();
+            var wc = new WebClient();
             wc.Proxy = null;
-            
-            var data = await wc.DownloadStringTaskAsync(new Uri("http://" + site.SiteName));
-
+            try
+            {
+                site.Data = await wc.DownloadStringTaskAsync(new Uri("http://" + site.SiteName));
+            }
+            catch(Exception ex)
+            {
+                Console.WriteLine("Site: {0} has errored!", site.SiteName);
+                site.Error = ex.ToString();
+            }
             site.Headers = wc.ResponseHeaders;
 
-            Console.WriteLine("Completed: " + site.SiteName);
+            Console.WriteLine("(T:{0}) Completed {1}", taskID, site.SiteName);
             return site;
         }
     }
